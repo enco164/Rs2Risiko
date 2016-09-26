cb65502376443e4ff3290c69723352704166b6a4
milanzp <milanzp@hotmail.com>
2016-09-27 01:52:04 +0200

    susedne teritorije, zadaci

--- app/src/main/java/com/rs2/risiko/data/Goal.java
+++ app/src/main/java/com/rs2/risiko/data/Goal.java
@@ -35,16 +35,34 @@ public class Goal implements Parcelable {
         goals.add(new Goal(2));
         goals.add(new Goal(3));
         goals.add(new Goal(4));
+        goals.add(new Goal(5));
+        goals.add(new Goal(6));
+        goals.add(new Goal(7));
+        goals.add(new Goal(8));
+        goals.add(new Goal(9));
+        goals.add(new Goal(10));
+        goals.add(new Goal(11));
+        goals.add(new Goal(12));
+        goals.add(new Goal(13));
 
         return goals;
     }
 
     final static String[] DESCRIPTIONS = {
-            "Unistiti zelenog",
-            "Unistiti plavog",
-            "Osvojiti ceo svet",
-            "Osvoji Severnu Ameriku i Afriku",
-            "Osvoji Aziju i Juznu Ameriku"
+            "Capture Europe, Australia and one other continent",
+            "Capture Europe, South America and one other continent",
+            "Capture North America and Africa",
+            "Capture North America and Australia",
+            "Capture Asia and South America",
+            "Capture Asia and Africa",
+            "Capture 24 territories",
+            "Capture 18 territories and occupy each with two troops",
+            "Destroy dark blue  or — if you are dark blue or dark blue is not in game — capture 24 territories",
+            "Destroy lite blue  or — if you are lite blue or lite blue is not in game— capture 24 territories",
+            "Destroy red  or — if you are red or red is not in game— capture 24 territories",
+            "Destroy black or — if you are black or black is not in game— capture 24 territories",
+            "Destroy yellow  or — if you are yellow or yellow is not in game— capture 24 territories",
+            "Destroy purple  or — if you are purple or purple is not in game— capture 24 territories"
     };
 
     // Parcelling part
@@ -71,4 +89,5 @@ public class Goal implements Parcelable {
             return new Goal[size];
         }
     };
+
 }
--- app/src/main/java/com/rs2/risiko/data/Territory.java
+++ app/src/main/java/com/rs2/risiko/data/Territory.java
@@ -146,4 +146,181 @@ public class Territory implements Parcelable{
         return territories;
 
     }
+
+    public static boolean areNeighboringTeritories(String t1, String t2) {
+        boolean[][] neighboringTeritories = new boolean[41][41];
+        neighboringTeritories[0][2] = true;
+        neighboringTeritories[0][12] = true;
+        neighboringTeritories[0][24] = true;
+        neighboringTeritories[1][2] = true;
+        neighboringTeritories[1][3] = true;
+        neighboringTeritories[1][4] = true;
+        neighboringTeritories[1][5] = true;
+        neighboringTeritories[1][6] = true;
+        neighboringTeritories[1][12] = true;
+        neighboringTeritories[2][0] = true;
+        neighboringTeritories[2][1] = true;
+        neighboringTeritories[2][4] = true;
+        neighboringTeritories[2][12] = true;
+        neighboringTeritories[3][1] = true;
+        neighboringTeritories[3][4] = true;
+        neighboringTeritories[3][5] = true;
+        neighboringTeritories[4][1] = true;
+        neighboringTeritories[4][2] = true;
+        neighboringTeritories[4][3] = true;
+        neighboringTeritories[4][13] = true;
+        neighboringTeritories[5][1] = true;
+        neighboringTeritories[5][3] = true;
+        neighboringTeritories[5][6] = true;
+        neighboringTeritories[5][7] = true;
+        neighboringTeritories[6][1] = true;
+        neighboringTeritories[6][5] = true;
+        neighboringTeritories[6][7] = true;
+        neighboringTeritories[6][12] = true;
+        neighboringTeritories[7][5] = true;
+        neighboringTeritories[7][6] = true;
+        neighboringTeritories[7][8] = true;
+        neighboringTeritories[8][7] = true;
+        neighboringTeritories[8][9] = true;
+        neighboringTeritories[8][11] = true;
+        neighboringTeritories[9][8] = true;
+        neighboringTeritories[9][10] = true;
+        neighboringTeritories[9][11] = true;
+        neighboringTeritories[9][31] = true;
+        neighboringTeritories[10][9] = true;
+        neighboringTeritories[10][11] = true;
+        neighboringTeritories[11][8] = true;
+        neighboringTeritories[11][9] = true;
+        neighboringTeritories[11][10] = true;
+        neighboringTeritories[12][0] = true;
+        neighboringTeritories[12][1] = true;
+        neighboringTeritories[12][2] = true;
+        neighboringTeritories[12][6] = true;
+        neighboringTeritories[13][4] = true;
+        neighboringTeritories[13][14] = true;
+        neighboringTeritories[13][15] = true;
+        neighboringTeritories[14][13] = true;
+        neighboringTeritories[14][15] = true;
+        neighboringTeritories[14][16] = true;
+        neighboringTeritories[15][13] = true;
+        neighboringTeritories[15][14] = true;
+        neighboringTeritories[15][17] = true;
+        neighboringTeritories[15][18] = true;
+        neighboringTeritories[16][14] = true;
+        neighboringTeritories[16][17] = true;
+        neighboringTeritories[16][31] = true;
+        neighboringTeritories[16][32] = true;
+        neighboringTeritories[17][15] = true;
+        neighboringTeritories[17][16] = true;
+        neighboringTeritories[17][18] = true;
+        neighboringTeritories[17][28] = true;
+        neighboringTeritories[18][15] = true;
+        neighboringTeritories[18][17] = true;
+        neighboringTeritories[18][19] = true;
+        neighboringTeritories[18][25] = true;
+        neighboringTeritories[18][28] = true;
+        neighboringTeritories[19][18] = true;
+        neighboringTeritories[19][20] = true;
+        neighboringTeritories[19][25] = true;
+        neighboringTeritories[19][26] = true;
+        neighboringTeritories[20][19] = true;
+        neighboringTeritories[20][21] = true;
+        neighboringTeritories[20][22] = true;
+        neighboringTeritories[20][23] = true;
+        neighboringTeritories[20][26] = true;
+        neighboringTeritories[21][20] = true;
+        neighboringTeritories[21][22] = true;
+        neighboringTeritories[21][24] = true;
+        neighboringTeritories[22][20] = true;
+        neighboringTeritories[22][21] = true;
+        neighboringTeritories[22][23] = true;
+        neighboringTeritories[22][24] = true;
+        neighboringTeritories[23][20] = true;
+        neighboringTeritories[23][22] = true;
+        neighboringTeritories[23][24] = true;
+        neighboringTeritories[23][26] = true;
+        neighboringTeritories[23][27] = true;
+        neighboringTeritories[24][21] = true;
+        neighboringTeritories[24][22] = true;
+        neighboringTeritories[24][23] = true;
+        neighboringTeritories[24][27] = true;
+        neighboringTeritories[24][0] = true;
+        neighboringTeritories[25][18] = true;
+        neighboringTeritories[25][19] = true;
+        neighboringTeritories[25][26] = true;
+        neighboringTeritories[25][28] = true;
+        neighboringTeritories[25][29] = true;
+        neighboringTeritories[26][19] = true;
+        neighboringTeritories[26][20] = true;
+        neighboringTeritories[26][23] = true;
+        neighboringTeritories[26][25] = true;
+        neighboringTeritories[26][29] = true;
+        neighboringTeritories[26][30] = true;
+        neighboringTeritories[27][23] = true;
+        neighboringTeritories[27][24] = true;
+        neighboringTeritories[28][17] = true;
+        neighboringTeritories[28][18] = true;
+        neighboringTeritories[28][25] = true;
+        neighboringTeritories[28][29] = true;
+        neighboringTeritories[28][32] = true;
+        neighboringTeritories[29][25] = true;
+        neighboringTeritories[29][26] = true;
+        neighboringTeritories[29][28] = true;
+        neighboringTeritories[29][30] = true;
+        neighboringTeritories[30][26] = true;
+        neighboringTeritories[30][29] = true;
+        neighboringTeritories[30][37] = true;
+        neighboringTeritories[31][9] = true;
+        neighboringTeritories[31][16] = true;
+        neighboringTeritories[31][32] = true;
+        neighboringTeritories[31][33] = true;
+        neighboringTeritories[31][34] = true;
+        neighboringTeritories[32][16] = true;
+        neighboringTeritories[32][28] = true;
+        neighboringTeritories[32][31] = true;
+        neighboringTeritories[32][33] = true;
+        neighboringTeritories[33][31] = true;
+        neighboringTeritories[33][32] = true;
+        neighboringTeritories[33][34] = true;
+        neighboringTeritories[33][35] = true;
+        neighboringTeritories[33][36] = true;
+        neighboringTeritories[34][31] = true;
+        neighboringTeritories[34][33] = true;
+        neighboringTeritories[34][35] = true;
+        neighboringTeritories[35][33] = true;
+        neighboringTeritories[35][34] = true;
+        neighboringTeritories[35][36] = true;
+        neighboringTeritories[36][33] = true;
+        neighboringTeritories[36][35] = true;
+        neighboringTeritories[37][30] = true;
+        neighboringTeritories[37][38] = true;
+        neighboringTeritories[37][39] = true;
+        neighboringTeritories[38][37] = true;
+        neighboringTeritories[38][40] = true;
+        neighboringTeritories[39][37] = true;
+        neighboringTeritories[39][40] = true;
+        neighboringTeritories[40][38] = true;
+        neighboringTeritories[40][39] = true;
+
+        int id1 = Integer.parseInt(t1.substring(3));
+        int id2 = Integer.parseInt(t2.substring(3));
+
+        return neighboringTeritories[id1][id2];
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
